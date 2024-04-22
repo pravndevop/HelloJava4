d4d91df8aaba9ab9f1e7cbe0f55ccf9fa572f81f
praveen <praveenarya518@gmail.com>
2024-04-22 21:40:17 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+        System.out.println(3+3);
+        System.out.println(3*3);
+
+        /*string*/String name="Praveen";
+        System.out.println(name);
+
+        /*int*/ int OddNum=99;
+        System.out.println(OddNum);
+
+        int EvenNum;
+        EvenNum=100;
+        System.out.println(EvenNum);
+
+        final int MyAge=23;
+//        MyAge=24;
+        System.out.println(MyAge);
+
+        /*char*/ char AlphaStart='A';
+        System.out.println(AlphaStart);
+
+        /*float*/ float PiNum=22.34f;
+        System.out.println(PiNum);
+
+        /*boolean*/ boolean NameIs=false;
+        System.out.println(NameIs);
+    }
+}
